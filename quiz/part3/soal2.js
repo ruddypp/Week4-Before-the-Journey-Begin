c9395ac70cc77c52a0690f44ed3376147d7a5d63
Rudy Paningal <paningalrudy@gmail.com>
2025-03-19 11:46:06 +0700

    week-4

--- quiz/part3/soal2.js
+++ quiz/part3/soal2.js
@@ -1,2 +1,12 @@
-const matches = str.match(/abc/g);
-  return matches ? matches.length : 0;
+  function cariPelaku(str) {
+    // you can only write your code here!let cari = str.match(/abc/g);
+   if(cari) return cari.length;
+   return 0;
+  }
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
